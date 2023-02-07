3bae72c2464fb9954ffcdc997a943ec6f7942ca8
NadezhdaBul <Bulygina_n_a@mail.ru>
2023-02-07 20:04:57 +0300

    Less1Java

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,5 +1,35 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double temp = 9.2;
+        boolean cold = temp <= 10;
+        if (cold) {
+
+            System.out.println("Смотреть сериал");
+        }
+        else {
+            System.out.println("Идем гулять");
+        }
+
+
+        int zarplata = 190_000;
+        double premiya = 135_000.6;
+        int rabotnikov = 1500;
+        double zarabotok_za_god = (zarplata + premiya)*12;
+        System.out.println(zarabotok_za_god);
+        int fot_mes = zarplata * rabotnikov;
+        System.out.println(fot_mes);
+
+//        переполнение
+        int fot_god = fot_mes * 12;
+        System.out.println(fot_god);
+
+
+//        String name = "Саша";
+//        System.out.println(name + " любит пить кофе");
+//        int age = 17;
+//        System.out.println(name + " ученик, ему " + age + " лет");
+
+
+
     }
 }
